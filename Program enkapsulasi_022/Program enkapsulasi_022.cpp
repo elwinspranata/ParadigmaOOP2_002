0daa704b584de4b90582de4a404cdad238f33b15
elwinspranata <elwinpranata02@gmail.com>
2023-05-31 16:46:49 +0700

    lampu teras  rumah

--- Program enkapsulasi_022/Program enkapsulasi_022.cpp
+++ Program enkapsulasi_022/Program enkapsulasi_022.cpp
@@ -13,3 +13,18 @@ public:
 	}
 }; 
 
+int main() {
+	remoteLampu lampuRumah;
+
+	lampuRumah.setSaklarNo(0, "Lampu Teras Rumah");
+	lampuRumah.setSaklarNo(1, "Lampu Ruang Tamu");
+	lampuRumah.setSaklarNo(2, "Lampu Lampu Kamar Tidur");
+	lampuRumah.setSaklarNo(3, "Lampu Lampu Dapur");
+
+	cout << lampuRumah.getsaklarNo(0) << endl;
+	cout << lampuRumah.getsaklarNo(1) << endl;
+	cout << lampuRumah.getsaklarNo(2) << endl;
+	cout << lampuRumah.getsaklarNo(3) << endl;
+
+	return 0;
+} 
